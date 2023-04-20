ff364c24b65cfcf7cbc51161a4a56343bdf79fad
sungyoung1107 <dong09161107@gmail.com>
2023-04-20 17:42:10 +0900

    2023-04-20

--- src/main/java/com/kbstar/controller/MainController.java
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/* ws0420 */
 @Controller
 public class MainController {
     @RequestMapping("/")
